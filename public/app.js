62b11c65b09ba031ad28653c2a6d8d0e059974bd
Kona Marie <ambermoreyra@gmail.com>
2019-08-25 20:37:26 -0600

    external link working

--- public/app.js
+++ public/app.js
@@ -8,19 +8,26 @@ $.getJSON("/articles", function (data) {
         // $(".brewers").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
 
         // $(".brewers").append("<div class='card'>");
-        $(".brewers").append("<div data-_id='" + data[i]._id + "' class='card'>");
-        $("[data-_id=" + data[i]._id + "]").append("<a class='article-link' target='_blank' href='mlb.com/brewers" + data[i].link + "'>" + data[i].title + "</a>").append("<a class='btn btn-danger delete'>" + 'Delete From Saved' + "</a>").append("<a class='btn btn-info notes'>" + 'Article Notes' + "</a>");
+        $(".brewers").append("<div data-_id='" + data[i]._id + "' class='card mb-4'>");
+        $("[data-_id=" + data[i]._id + "]").append("<h3 data-id=" + data[i]._id + " class='card-header card-title'>");
+        $("[data-id=" + data[i]._id + "]").append("<a class='article-link' target='_blank' href='https://www.mlb.com/brewers" + data[i].link + "'>" + data[i].title + "</a>").append("<a class='btn btn-info notes float-right'>" + 'Article Notes' + "</a>");
+        // append("<a class='btn btn-danger delete ml-4 float-right'>" + 'Delete From Saved' + "</a>").append("<a class='btn btn-info notes float-right'>" + 'Article Notes' + "</a>");
 
     }
 });
 
+// $(document).on("click", ".save", function () {
+//     BrewersSchema.saved = true;
+//     console.log(BrewersSchema);
+// }
+// );
 
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function () {
+$(document).on("click", ".notes", function () {
     // Empty the notes from the note section
     $("#notes").empty();
     // Save the id from the p tag
-    var thisId = $(this).attr("data-id");
+    var thisId = $(".card-header").attr("data-id");
 
     // Now make an ajax call for the Article
     $.ajax({
@@ -34,8 +41,11 @@ $(document).on("click", "p", function () {
             $("#notes").append("<h2>" + data.title + "</h2>");
             // An input to enter a new title
             $("#notes").append("<input id='titleinput' name='title' >");
+            $("#notes").append("<br>");
+
             // A textarea to add a new note body
             $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+            $("#notes").append("<br>");
             // A button to submit a new note, with the id of the article saved to it
             $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
